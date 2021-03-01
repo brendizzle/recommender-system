85674cdd770b5efc9ed25ecd050e24cba3b2073d
Brendan Noone <brendan.noone95@gmail.com>
2021-03-01 13:59:51 -0500

    commit on main

--- test.py
+++ test.py
@@ -1,5 +1,28 @@
+import csv
+
+def readfile(filepath):
+    cities = []
+    with open(filepath) as newfile:
+        csvreader = csv.reader(newfile, delimiter=',')
+        for city in csvreader:
+            parsed_data = ';'.join(city)
+            s = parsed_data.split(";")
+            #print(s)
+            cities.append(s)
+
+    arr = [row[0:3] for row in cities]
+    return arr
+
+
 def main():
-    print("Hello World!")
+    a = readfile('1000-largest-us-cities.csv')
+    print(a)
+
+    with open('final_weather_data.csv') as csvfile:
+        weatherreader = csv.reader(csvfile, delimiter=',')
+        for row in weatherreader:
+            parsed_data = ', '.join(row)
+            item = row[2]
 
 if __name__ == "__main__":
     main()
